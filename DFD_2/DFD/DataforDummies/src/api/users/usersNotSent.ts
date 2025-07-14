4ef846ed0c802bfba365439591ef3f4a0f3bff17
Jolesito123 <rodriguez.ramirez.joseluis12@gmail.com>
2025-07-14 17:55:29 -0600

    Delete DFD_2/DFD/DataforDummies directory

--- DFD_2/DFD/DataforDummies/src/api/users/usersNotSent.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from "../axiosInstance"
-// Fetch de usuarios en search
-export const fetchUsers = async () => {
-    const response = await axiosInstance.get("/contacts/search/")
-    return response.data
-    // setUsers(response.data)
-    // setFilteredUsers(response.data)
-}
--- DFD_2/DFD/DataforDummies/src/api/users/usersPendingReceived.ts
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "../axiosInstance"
-import { User } from "../auth"
-//Fetch de contactos con solicitudes pendientes que te hayan mandado
-export const fetchUsersPendingReceived = async (): Promise<User[]> => {
-    const response = await axiosInstance.get("/contacts/pending-received/")
-    return response.data
-}
