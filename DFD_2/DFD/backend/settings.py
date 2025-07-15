2291dcd9fef38dfe4c8276243fac015b91daa32d
Jolesito123 <rodriguez.ramirez.joseluis12@gmail.com>
2025-07-15 00:57:41 -0600

    Update settings.py

--- DFD_2/DFD/backend/settings.py
+++ DFD_2/DFD/backend/settings.py
@@ -131,6 +131,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'DataforDummies.middleware.request_logger.RequestLoggingMiddleware',  # <- este
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
